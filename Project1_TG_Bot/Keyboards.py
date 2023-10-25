b357e8162be2ceddfde47cc6f8630490c8e7aa79
unknown <rukhlya1999@mail.ru>
2023-10-25 22:28:14 +0300

    new_edit

--- Project1_TG_Bot/Keyboards.py
+++ Project1_TG_Bot/Keyboards.py
@@ -4,16 +4,15 @@ import random
 keyboard_reply = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True)
 but_help = KeyboardButton(text="/help")
-but_rand = KeyboardButton(text="Random photo")
+but_rand = KeyboardButton(text="/Random_photo")
 but_description = KeyboardButton(text="/description")
 keyboard_reply.add(but_help).insert(but_description).row(but_rand)
 ###########################################################
-open_key_photo = ReplyKeyboardMarkup(resize_keyboard=True)
+open_key_photo = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
 but_rand_photo = KeyboardButton(text="Рандом")
 but_return = KeyboardButton(text="Главное меню")
 
 open_key_photo.add(but_rand_photo, but_return)
-
 
 key_inline_photo = InlineKeyboardMarkup(row_width=2)
 but_next_photo = InlineKeyboardButton(
--- Project1_TG_Bot/Project1.py
+++ Project1_TG_Bot/Project1.py
@@ -1,102 +1,77 @@
-from aiogram import types, Bot, executor, Dispatcher
+from aiogram import Dispatcher,types,executor,Bot
+from Keyboards import keyboard_reply,key_inline_photo,open_key_photo
 import random
-from Token import api_token
-from Keyboards import keyboard_reply, open_key_photo, key_inline_photo
+from aiogram.types import InlineKeyboardButton,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardMarkup
+from  Token import api_token
+from icecream import ic
 from aiogram.dispatcher.filters import Text
 
-
-
-bot = Bot(api_token)
-dp = Dispatcher(bot)
-
+bot=Bot(api_token)
+dp=Dispatcher(bot)
 
 async def on_start(_):
-    print("Я был запущен")
-
-HELP_COMMAND = """
-<b>/start</b> - <em>Приветствие бота</em>
-<b>/help</b> - <em>Список команд</em>
-<b>/description</b> - <em>Описание бота</em>
+    ic("Success!!")
 
 
+HELP_COMMAND="""
+
+<b>/start</b> - <em>приветствие бота</em>
+<b>/description</b> - <em>описание бота</em>
+<b>/help</b> - <em>список команд</em>
 
 """
 
-async def send_rand(message:types.Message):
-    random_photo=random.choice(list(photos.keys()))
-    await bot.send_photo(message.chat.id,photo=random_photo,reply_markup=key_inline_photo)
-
-
-
-
-list_photo = ["https://w.forfun.com/fetch/c5/c514ddd3da0d86f1348f4b10560f7f35.jpeg",
-              "https://fikiwiki.com/uploads/posts/2022-02/1644918620_17-fikiwiki-com-p-krasivie-kartinki-visokogo-razresheniya-19.jpg"]
-
-
-photos=dict(zip(list_photo,["Enviroment","Paris"]))
-
-random_photo=random.choice(list(photos.keys()))
-
-
-
-@dp.message_handler(Text(equals="Random photo"))
-async def open_keyboard_photo(message: types.Message):
-    await message.answer(text="Чтобы отправить рандомную фотку, нажмите random",
-                         reply_markup=open_key_photo)
-
-
-@dp.message_handler(Text(equals="Главное меню"))
-async def open_keyboard(message: types.Message):
-    await message.answer(text="Добро пожаловать в главное меню",
-                         reply_markup=keyboard_reply)
-
-
-
-
-@dp.message_handler(Text(equals="Рандом"))
-async def open_keyboard(message: types.Message):
-    await bot.send_photo(chat_id=message.chat.id, photo=random.choice(list_photo))
-    await bot.send_message(message.from_user.id, "Какая крутая фотка!!", reply_markup=key_inline_photo)
-
+list_ex=["https://proprikol.ru/wp-content/uploads/2020/04/krasivye-kartinki-vysokogo-razresheniya-3.jpg","https://w.forfun.com/fetch/03/03f8cd3f6796daaacc1fe43ffb7704b7.jpeg"]
+dict_photos=dict(zip(list_ex,["first","second"]))
+random_photo=random.choice(list(dict_photos.keys()))
 
 @dp.message_handler(commands=["start"])
-async def start(message: types.Message):
-    await message.answer("Добро пожаловать в нашего бота!!", reply_markup=keyboard_reply)
+async def start(message:types.Message):
+    await message.answer("Привет, рад тебя видеть в нашем боте!!!",reply_markup=keyboard_reply)
 
 
 @dp.message_handler(commands=["help"])
-async def help(message: types.Message):
-    await bot.send_message(message.from_user.id, HELP_COMMAND, parse_mode="html")
+async def help(message:types.Message):
+    await message.answer(HELP_COMMAND,parse_mode="html")
 
 
 @dp.message_handler(commands=["description"])
-async def descript(message: types.Message):
-    await message.answer("Наш бот умеет отправлять случайные фотографии!!")
-    await bot.send_sticker(message.chat.id, sticker="CAACAgIAAxkBAAEKcA1lGvSncg2AAAFddw2C8aDtktVtmj4AApoMAALvndlLqU1EIZoqq3kwBA")
-    
+async def descript(message:types.Message):
+    await message.answer("Наш бот умеет отправлять рандомные фотографии и стикеры!!")
+    await bot.send_sticker(message.chat.id,sticker="CAACAgIAAxkBAAEKcA1lGvSncg2AAAFddw2C8aDtktVtmj4AApoMAALvndlLqU1EIZoqq3kwBA")
 
 
+@dp.message_handler(commands=["Random_photo"])
+async def rand_photo(message:types.Message):
+    await message.answer("Чтобы получить рандомную фотку необходимо отправить команду Рандом",reply_markup=open_key_photo)
+
+
+@dp.message_handler(Text(equals="Рандом"))
+async def rand(message:types.Message):
+    random_photo=random.choice(list(dict_photos.keys()))
+    await bot.send_photo(message.from_user.id,random_photo,caption=dict_photos[random_photo],reply_markup=key_inline_photo)
+
+
+@dp.message_handler(Text(equals="Главное меню"))
+async def return_main_menu(message:types.Message):
+    await message.answer("Вы вернулись в главное меню",reply_markup=keyboard_reply)
 
 
 @dp.callback_query_handler()
-async def proccess_callback(callback: types.callback_query):
+
+async def vote(callback:types.CallbackQuery):
     global random_photo
     if callback.data=="like":
-        await callback.answer("Вам понравилось")
-    elif callback.data=="dislike":
-        await callback.answer("Вам не понравилось")
+        await callback.message.answer("Лайк этой фотки")
+    elif callback.data =="dislike":
+        await callback.message.answer("Дизлайк этой фотки")
     else:
-        
-        random_photo=random.choice(list(filter(lambda x:x!=random_photo,list(photos.keys()))))
-        await callback.message.edit_media(types.InputMedia(media=random_photo,
-                                                           type="photo",caption=photos[random_photo],
-                                                           reply_markup=key_inline_photo))
-        await callback.answer()
+        random_photo=random.choice(list(filter(lambda x:x!=random_photo,list(dict_photos.keys()))))
+        await bot.edit_message_text(chat_id=callback.message.chat.id,photo=random_photo,caption=dict_photos[random_photo]
+                             ,reply_markup=key_inline_photo)
 
 
 
-        
-        
-if __name__ == "__main__": 
-    executor.start_polling(dp, on_startup=on_start, skip_updates=True)
-    raise SystemExit(0)
+
+if __name__=="__main__":
+    executor.start_polling(dp,skip_updates=True,on_startup=on_start)
